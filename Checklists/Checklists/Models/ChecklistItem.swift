7659ce404519f11363dc4077555195b61d76bdc9
romaric roussel <romaric.roussel@outlook.fr>
2019-02-14 14:27:15 +0100

    7

---
+++ Checklists/Checklists/Models/ChecklistItem.swift
@@ -0,0 +1,23 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by lpiem on 14/02/2019.
+//  Copyright © 2019 lpiem. All rights reserved.
+//
+
+import Foundation
+
+struct ChecklistItem {
+    var text: String
+    var checked: Bool?
+    
+    
+    init(text:String, checked:Bool = false){
+        self.text = text
+        self.checked = checked
+    }
+}
+
+
+
