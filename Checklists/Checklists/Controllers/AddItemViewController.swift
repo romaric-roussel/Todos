87dc11caaf0516349412e1b21a21d781ea59743c
romaric roussel <romaric.roussel@outlook.fr>
2019-02-21 10:14:56 +0100

    19

--- Checklists/Checklists/Controllers/AddItemViewController.swift
+++ Checklists/Checklists/Controllers/AddItemViewController.swift
@@ -9,5 +9,15 @@
 import UIKit
 
 class AddItemViewController: UITableViewController {
+    
+    
+     @IBAction func cancel() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+     @IBAction func done() {
+        dismiss(animated: true, completion: nil)
 
+    }
+    
 }
