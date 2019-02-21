91cc747da44573a44b55c9e40ed5f1fcb0779dcc
romaric roussel <romaric.roussel@outlook.fr>
2019-02-21 16:38:55 +0100

    32/33

---
+++ Checklists/Checklists/Controllers/ChecklistItemCell.swift
@@ -0,0 +1,15 @@
+//
+//  ChecklistItemCell.swift
+//  Checklists
+//
+//  Created by lpiem on 21/02/2019.
+//  Copyright © 2019 lpiem. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistItemCell: UITableViewCell {
+
+    @IBOutlet weak var lblibelle: UILabel!
+    @IBOutlet weak var lbChecked: UILabel!
+}
